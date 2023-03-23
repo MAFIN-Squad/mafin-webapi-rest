5424c3d425e57f24d93a422849e97dce7bdde1c8
Yauhen Klimiashuk <Yauhen_Klimiashuk@epam.com>
2023-03-23 03:27:22 +0100

    Fix capitalization

--- src/Mafin.Web.Api.Rest/Authentication/BasicAuthHandler.cs
+++ src/Mafin.Web.Api.Rest/Authentication/BasicAuthHandler.cs
@@ -8,21 +8,21 @@ namespace Mafin.Web.Api.Rest.Authentication;
 /// </summary>
 public class BasicAuthHandler : BaseAuthHandler
 {
-    private readonly string _username;
+    private readonly string _userName;
     private readonly string _password;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="BasicAuthHandler"/> class.
     /// </summary>
-    /// <param name="username">Username value.</param>
+    /// <param name="userName">UserName value.</param>
     /// <param name="password">user password value.</param>
-    public BasicAuthHandler(string username, string password)
+    public BasicAuthHandler(string userName, string password)
     {
-        _username = username;
+        _userName = userName;
         _password = password;
     }
 
     /// <inheritdoc cref="BaseAuthHandler.GetAuthHeaderValue"/>
     protected override AuthenticationHeaderValue GetAuthHeaderValue() =>
-        new("Basic", Convert.ToBase64String(Encoding.Default.GetBytes($"{_username}:{_password}")));
+        new("Basic", Convert.ToBase64String(Encoding.Default.GetBytes($"{_userName}:{_password}")));
 }
