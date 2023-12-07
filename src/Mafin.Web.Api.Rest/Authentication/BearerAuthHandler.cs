0509584d1101ff4ebc74d445950836bba6a69e91
Yauhen Klimiashuk <Yauhen_Klimiashuk@epam.com>
2023-12-07 19:52:04 +0100

    Add code style changes

--- src/Mafin.Web.Api.Rest/Authentication/BearerAuthHandler.cs
+++ src/Mafin.Web.Api.Rest/Authentication/BearerAuthHandler.cs
@@ -5,20 +5,12 @@ namespace Mafin.Web.Api.Rest.Authentication;
 /// <summary>
 /// Bearer Authentication handling type.
 /// </summary>
-public class BearerAuthHandler : BaseAuthHandler
+/// <remarks>
+/// Initializes a new instance of the <see cref="BearerAuthHandler"/> class.
+/// </remarks>
+/// <param name="tokenProvider">Bearer token provider implementation.</param>
+public class BearerAuthHandler(IBearerTokenProvider tokenProvider) : BaseAuthHandler
 {
-    private readonly IBearerTokenProvider _tokenProvider;
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="BearerAuthHandler"/> class.
-    /// </summary>
-    /// <param name="tokenProvider">Bearer token provider implementation.</param>
-    public BearerAuthHandler(IBearerTokenProvider tokenProvider)
-    {
-        _tokenProvider = tokenProvider;
-    }
-
     /// <inheritdoc cref="BaseAuthHandler.GetAuthHeaderValue"/>
-    protected override AuthenticationHeaderValue GetAuthHeaderValue() =>
-        new("Bearer", _tokenProvider.GetBearerToken());
+    protected override AuthenticationHeaderValue GetAuthHeaderValue() => new("Bearer", tokenProvider.GetBearerToken());
 }
